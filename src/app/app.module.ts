7ff802c270cdcff4afd14a03d3237f328a920bbe
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-04 11:49:58 +0100

    feat(app): add functionality to switch to dark mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ToolbarModule,InputSwitchModule, FormsModule, ButtonModule, InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  checked: boolean = true;
+  selectedTheme: string = 'dark';
+  themeService: ThemeService = inject(ThemeService);
+
+  ngOnInit(): void {
+    this.themeService.setTheme(this.selectedTheme);
+  }
+
+  onThemeChange(theme: string): void {
+    this.selectedTheme = theme;
+    this.themeService.setTheme(theme);
+  }
+}
